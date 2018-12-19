7104a4999eed1e548e24d64c6807c6b92e3df000
Ankit <ankit.rajput19a@gmail.com>
2018-12-20 02:30:34 +0530

    added connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -11,12 +11,20 @@ const port = process.env.PORT || 3000;
 
 var server= http.createServer(app);
 
+var io = socketIO(server);
 app.use(express.static(publicPath));
 //
 // app.get('/',(req,res)=>{
 //     res.render('index.html');
 // });
 // automatically rendered index.html
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+
+    socket.on('disconnect',()=>{
+      console.log('user disconnected');
+      });
+});
 
 server.listen(port,()=>{
   console.log( `server is up on port: ${port}`);
