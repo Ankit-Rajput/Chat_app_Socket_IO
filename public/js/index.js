c778ea798f85108c329e61aea0ca4ee752b9f0d6
Ankit <ankit.rajput19a@gmail.com>
2018-12-22 00:42:05 +0530

    added autoScroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 // removed arrow functions from client side to make it work on all browsers
+function scrollToBottom(){
+    var messages= jQuery('#messages');
+    var newMessage= messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop= messages.prop('scrollTop');
+    var scrollHeight= messages.prop('scrollHeight');
+    var newMessageHeight= newMessage.innerHeight();
+    var lastMessageHeight= newMessage.prev().innerHeight();
+
+    if(clientHeight+ scrollTop+ newMessageHeight+ lastMessageHeight >=scrollHeight)
+        messages.scrollTop(scrollHeight);
+}
+
+
 socket.on('connect',function (){
   console.log('connected to server');
 });
@@ -16,6 +30,7 @@ socket.on('newMessage',function(message){ // server to client
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -27,6 +42,7 @@ socket.on('newLocationMessage', function(message){
       createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit',function(e){  //e for event
